6b08596ff64cdf2dda5ef99d68c490682fdc7a80
David Ernesto Tamo Turpo <Parzivall@users.noreply.github.com>
2021-12-14 14:09:18 -0500

    Eliminando comentarios TODO  de Spinner.js
    // TODO Implement GIF-based fallback for IE9 and another non-animating browsers
    //      See https://github.com/tobiasahlin/SpinKit for how-to

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,7 +1,4 @@
 var React = require('react')
-
-// TODO Implement GIF-based fallback for IE9 and another non-animating browsers
-//      See https://github.com/tobiasahlin/SpinKit for how-to
 var Spinner = React.createClass({
   getDefaultProps() {
     return {size: 6, spacing: 2}
