16f5462e42c2e6cdedc938dbcd480c4a897eb48b
angel <andresbjar97@gmail.com>
2021-12-27 21:07:01 -0500

    var declarancion ambito de la funcion local

--- src/utils/extend.js
+++ src/utils/extend.js
@@ -5,7 +5,7 @@ function extend(dest, src1, src2) {
   }
   if (src2) {
     props = Object.keys(src2)
-    for (i = 0, l = props.length; i < l; i++) {
+    for (var i = 0, l = props.length; i < l; i++) {
       dest[props[i]] = src2[props[i]]
     }
   }
