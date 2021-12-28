88c5f73a0fde7eda27fec8bfb936fa5ce6d39a1a
angel <andresbjar97@gmail.com>
2021-12-27 21:11:46 -0500

    mejorando ambito

--- src/utils/extend.js
+++ src/utils/extend.js
@@ -1,6 +1,7 @@
 function extend(dest, src1, src2) {
   var props = Object.keys(src1)
-  for (var i = 0, l = props.length; i < l; i++) {
+  var i;
+  for (i = 0, l = props.length; i < l; i++) {
     dest[props[i]] = src1[props[i]]
   }
   if (src2) {
