02439ef89d13fd0a7586b5ced6b79ffed1aada05
angel <andresbjar97@gmail.com>
2021-12-27 20:50:30 -0500

    delete commet

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,6 +1,6 @@
-var React = require('react');
+var React = require('react')
 
-// todo Implement GIF-based fallback for IE9 and another non-animating browsers
+
 //      See https://github.com/tobiasahlin/SpinKit for how-to
 var Spinner = React.createClass({
   getDefaultProps() {
