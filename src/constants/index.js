353774d4821da960f43a798be8b020b22ec1343f
angel <andresbjar97@gmail.com>
2021-12-29 04:02:19 -0500

    agregar 1 archivo con los parametros de increment

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,22 +1,2 @@
-import {hashHistory} from 'react-router'
-
-require('./style.css')
-
-require('setimmediate')
-//comentario
-var React = require('react')
-var {render} = require('react-dom')
-var Router = require('react-router/lib/Router')
-var useScroll = require('react-router-scroll/lib/useScroll')
-var applyRouterMiddleware = require('react-router/lib/applyRouterMiddleware')
-
-var routes = require('./routes').default
-
-render(
-  <Router
-    history={hashHistory}
-    render={applyRouterMiddleware(useScroll())}
-    routes={routes}
-  />,
-  document.getElementById('app')
-)
+export const MAX_STORIES = 500;
+export const STORY_INCREMENT = 30;
