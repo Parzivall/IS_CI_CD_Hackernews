b602c128308183aeb4bae460b85a1935fd3b6111
David Ernesto Tamo Turpo <Parzivall@users.noreply.github.com>
2021-12-30 13:57:35 -0500

    Añadiendo una prueba Unitaria
    Comprobamos que la fecha sea renderizada correctamente

--- src/__tests__/Story.spec.js
+++ src/__tests__/Story.spec.js
@@ -22,5 +22,6 @@ test('renders the story component with content', async () => {
     expect(getByTestId('story')).toBeTruthy(),
     expect(getByText('Tarnished: Google Responds')).toBeTruthy(),
     expect(getByTestId('story-by').textContent).toEqual('By: Karl Hadwen'),
+    expect(getByText('story-time').textContent).toEqual('1567209822'),
   ]);
 });
